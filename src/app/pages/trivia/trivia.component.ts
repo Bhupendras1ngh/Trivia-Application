eb7ba1a57cecef6fe8869fce76f95e6c34aa3715
Bhupendra <bhupendra@gamil.com>
2023-02-11 08:05:48 +0530

    added the CSS login of choosing correct/incorrect answer

--- src/app/pages/trivia/trivia.component.ts
+++ src/app/pages/trivia/trivia.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Route, Router } from '@angular/router';
 import { TriviaService } from 'src/app/services/trivia.service';
 
 @Component({
@@ -7,10 +8,11 @@ import { TriviaService } from 'src/app/services/trivia.service';
   styleUrls: ['./trivia.component.css']
 })
 export class TriviaComponent {
- constructor(public triviaService : TriviaService){}
+ constructor(public triviaService : TriviaService ,private router :Router){}
 
  questionIndex = 0;
-
+ userAnser ="";
+ score =0;
 
 
 
@@ -23,8 +25,25 @@ export class TriviaComponent {
       this.triviaService.questionData[this.questionIndex].incorrect_answers.push(this.triviaService.questionData[this.questionIndex].correct_answer);
       this.triviaService.questionData[this.questionIndex].incorrect_answers.sort(()=>0.5 - Math.random());
       console.log(this.triviaService.questionData[this.questionIndex].incorrect_answers)
-      
     }
   )
+ }
+
+ selectAnswer(option:string){
+   this.userAnser =option
+  if(this.triviaService.questionData[this.questionIndex].correct_answer ==option){
+    this.score +=5;
+    setTimeout(()=>{
+      this.userAnser ="";
+      this.questionIndex++;
+      this.triviaService.questionData[this.questionIndex].incorrect_answers.push(this.triviaService.questionData[this.questionIndex].correct_answer);
+      this.triviaService.questionData[this.questionIndex].incorrect_answers.sort(()=>0.5 - Math.random());
+    } ,2000) 
+  }
+  else{
+    setTimeout(()=>{
+    this.router.navigate(['/home'])
+    }, 2000)
+  }
  }
 }
